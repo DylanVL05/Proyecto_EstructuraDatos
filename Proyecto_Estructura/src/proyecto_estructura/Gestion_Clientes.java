98b75be7c0fef5c7de9ae5729d37199f749261cb
DylanVL05 <127649711+DylanVL05@users.noreply.github.com>
2023-10-13 23:49:42 -0600

    gestion clientes
    agregado prueba txt con bufer

--- Proyecto_Estructura/src/proyecto_estructura/Gestion_Clientes.java
+++ Proyecto_Estructura/src/proyecto_estructura/Gestion_Clientes.java
@@ -3,12 +3,79 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package proyecto_estructura;
+    
 
+
+
+
+
+
+  //  import java.io.BufferedWriter;
+//import java.io.FileWriter;
+//import java.io.IOException;
 /**
  *
  * @author andro
  */
 public class Gestion_Clientes {
+    
+
+    
+    
+    
+    
+    
+    
+//Prueba txt con buffer
+    
+/*
+    public static void guardarClienteEnArchivo(String nombre, String apellidos, String identificacion, String correo, String tel) {
+        Cliente cliente = new Cliente(nombre, apellidos, identificacion, correo, tel);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("clientes.txt", true))) {
+            writer.write(cliente.toString());
+            writer.newLine();
+            System.out.println("Cliente guardado en el archivo clientes.txt.");
+        } catch (IOException e) {
+            System.err.println("Error al guardar el cliente en el archivo: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        // Ejemplo de cómo llamar a guardarClienteEnArchivo
+        String nombre = "John";
+        String apellidos = "Doe";
+        String identificacion = "12345";
+        String correo = "john.doe@example.com";
+        String tel = "123-456-7890";
+
+        // Llamada a la función para guardar el cliente en el archivo
+        guardarClienteEnArchivo(nombre, apellidos, identificacion, correo, tel);
+        
+        // Puedes llamar a la función nuevamente con diferentes datos si deseas guardar más clientes.
+        String nombre2 = "Jane";
+        String apellidos2 = "Smith";
+        String identificacion2 = "67890";
+        String correo2 = "jane.smith@example.com";
+        String tel2 = "987-654-3210";
+
+       guardarClienteEnArchivo(nombre2, apellidos2, identificacion2, correo2, tel2);
+    }
+    
+    */
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
