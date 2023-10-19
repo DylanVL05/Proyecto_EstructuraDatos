bab5e5fbc88ada77101d089776784c282fbc990f
DylanVL05 <127649711+DylanVL05@users.noreply.github.com>
2023-10-19 01:03:11 -0600

    Implementacion de Funciones Gestion Clientes

--- Proyecto_Estructura/src/proyecto_estructura/Cliente.java
+++ Proyecto_Estructura/src/proyecto_estructura/Cliente.java
@@ -4,12 +4,23 @@
  */
 package proyecto_estructura;
 
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
 /**
  *
  * @author andro
  */
-public class Cliente {
-    
+public class Cliente implements Serializable{
+
+ 
      private String nombre, apellidos , identificacion , correo, tel ;
 
     public Cliente(String nombre, String apellidos, String identificacion, String correo, String tel) {
@@ -66,11 +77,117 @@ public class Cliente {
     }
 
  
-     
+         public static boolean GuardarCliente(Cliente[] vehiculos){
+          try {
+            String filename = "Cliente.DYLAN"; //Nombre archivo.extension
+            FileOutputStream fileOut = new FileOutputStream(filename);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(vehiculos);
+            out.close();
+            fileOut.close();
+            File file = new File(filename);
+            String absolutePath = file.getAbsolutePath();
+            System.out.println("File saved at: " + absolutePath);
+            System.out.println("Cliente object has been serialized and saved to " + filename + ":");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+    
+    
+      public static Cliente[] leerVehiculos(){
+        try {
+            String filename = "vehiculo.DYLAN";  
+            FileInputStream fileIn = new FileInputStream(filename);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+
+            Cliente[] clientes = (Cliente[]) in.readObject();
+            in.close();
+            fileIn.close();
+            return clientes;
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }    
+         
+    
+      
+          public static Cliente[] leerClientes(){
+        try {
+            String filename = "Cliente.DYLAN";  
+            FileInputStream fileIn = new FileInputStream(filename);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+
+            Cliente[] clientes = (Cliente[]) in.readObject();
+            in.close();
+            fileIn.close();
+            return clientes;
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+  public static void main(String[] args) {
+    // Crear un array de objetos Cliente para probar
+    Cliente[] clientes = new Cliente[2];
+    clientes[0] = new Cliente("Nombre1", "Apellido1", "12345", "correo1@example.com", "123456789");
+    clientes[1] = new Cliente("Nombre2", "Apellido2", "67890", "correo2@example.com", "987654321");
+
+    // Guardar los objetos Cliente en un archivo
+    if (GuardarCliente(clientes)) {
+        System.out.println("Clientes guardados correctamente.");
+    } else {
+        System.out.println("Error al guardar los clientes.");
+    }
+
+    // Leer los objetos Cliente desde el archivo
+    Cliente[] clientesGuardados = leerClientes();
+    if (clientesGuardados != null) {
+        System.out.println("Clientes leídos desde el archivo:");
+        for (Cliente cliente : clientesGuardados) {
+            System.out.println(cliente);
+        }
+    } else {
+        System.out.println("Error al leer los clientes desde el archivo.");
+        
+    }
+    
+    Gestion_Clientes.listarClientesRegistrados();
     
     
     
-    
+}
+
+        
+  
+  
+        
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
     
     
     
--- Proyecto_Estructura/src/proyecto_estructura/Gestion_Clientes.java
+++ Proyecto_Estructura/src/proyecto_estructura/Gestion_Clientes.java
@@ -4,6 +4,8 @@
  */
 package proyecto_estructura;
     
+import javax.swing.JOptionPane;
+
 
 
 
@@ -21,65 +23,43 @@ public class Gestion_Clientes {
     
 
     
+ 
     
-    
-    
-    
-    
-//Prueba txt con buffer
-    
-/*
-    public static void guardarClienteEnArchivo(String nombre, String apellidos, String identificacion, String correo, String tel) {
-        Cliente cliente = new Cliente(nombre, apellidos, identificacion, correo, tel);
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("clientes.txt", true))) {
-            writer.write(cliente.toString());
-            writer.newLine();
-            System.out.println("Cliente guardado en el archivo clientes.txt.");
-        } catch (IOException e) {
-            System.err.println("Error al guardar el cliente en el archivo: " + e.getMessage());
+    
+    
+       public static void listarClientesRegistrados() {
+        // Leer los clientes desde el archivo
+        Cliente[] clientesGuardados = Cliente.leerClientes();
+
+        if (clientesGuardados != null) {
+            System.out.println("Clientes registrados:");
+            for (Cliente cliente : clientesGuardados) {
+                JOptionPane.showMessageDialog(null,cliente);
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "No se encontraron");
+            
+            
+            
+            
         }
     }
 
-    public static void main(String[] args) {
-        // Ejemplo de cómo llamar a guardarClienteEnArchivo
-        String nombre = "John";
-        String apellidos = "Doe";
-        String identificacion = "12345";
-        String correo = "john.doe@example.com";
-        String tel = "123-456-7890";
-
-        // Llamada a la función para guardar el cliente en el archivo
-        guardarClienteEnArchivo(nombre, apellidos, identificacion, correo, tel);
-        
-        // Puedes llamar a la función nuevamente con diferentes datos si deseas guardar más clientes.
-        String nombre2 = "Jane";
-        String apellidos2 = "Smith";
-        String identificacion2 = "67890";
-        String correo2 = "jane.smith@example.com";
-        String tel2 = "987-654-3210";
-
-       guardarClienteEnArchivo(nombre2, apellidos2, identificacion2, correo2, tel2);
-    }
-    
-    */
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
 }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
