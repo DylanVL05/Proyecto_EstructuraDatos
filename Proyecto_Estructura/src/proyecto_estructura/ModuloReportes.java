e93f2d6dbdd1a344d0b0b6e8128781860865ec3d
LuisCarlosQuirosObando <lquiros460787@ufide.ac.cr>
2023-10-24 23:00:42 -0600

    commit 4
    Consulta Generación informes

--- Proyecto_Estructura/src/proyecto_estructura/ModuloReportes.java
+++ Proyecto_Estructura/src/proyecto_estructura/ModuloReportes.java
@@ -3,7 +3,6 @@
 import Cliente.Lista_Clientes;
 import Vehiculo.ListaVehiculos;
 import javax.swing.JOptionPane;
-
 import Vehiculo.ListaVehiculos;
 import javax.swing.JOptionPane;
 import proyecto_estructura.InformacionEmpresa;
@@ -34,6 +33,8 @@ public class ModuloReportes {
             
             op=Integer.parseInt(JOptionPane.showInputDialog(menu));
             switch(op){
+          
+                
             case 1: //cantidad de vehiculos
                 int submenuSeleccion = -1; // Inicializa la selección del submenú con un valor no válido
 
@@ -43,25 +44,50 @@ public class ModuloReportes {
                         switch (submenuSeleccion) {
                             case 0: // Vehiculos vendidos
                               ListaVehiculos vendidos=ListaVehiculos.leerVehiculos();
-                             vendidos.getCabeza().getDato().getEstado();
+                       //      vendidos.getCabeza().getDato().getEstado();
+                              if (vendidos.getCabeza().getDato().getEstado() == null) {
+                                    System.out.println("La lista está vacía.");
+                                 return;
+                                }if (vendidos.getCabeza().getDato().getEstado() == "Vendido"){
+                                    System.out.println();
+                                }
+                                    
                               
                                 break;
                             case 1:// Vehiculos reservados
                                ListaVehiculos reservados=ListaVehiculos.leerVehiculos();
+                                if (reservados.getCabeza().getDato().getEstado() == null) {
+                                    System.out.println("La lista está vacía.");
+                                 return;
+                                }if (reservados.getCabeza().getDato().getEstado() == "Reservado"){
+                                    System.out.println();
+                                }
                                
-                               
+                              
+             
                                 break;
                             case 2: // Vehiculos disponibles
                                ListaVehiculos disponibles=ListaVehiculos.leerVehiculos();
+                                if (disponibles.getCabeza().getDato().getEstado() == null) {
+                                    System.out.println("La lista está vacía.");
+                                 return;
+                                }if (disponibles.getCabeza().getDato().getEstado() == "Disponible"){
+                                    System.out.println();
+                                }
+                               
+                               
                                 break;            
                             case 3: // MostrarInfo
                                 InformacionEmpresa.mostrarInformacionEnDialogo();
                                 break;
-                            case 4: // Salir
-                                return;
+                            case 4: // volver
+                                break;
+                            default:
+                                JOptionPane.showMessageDialog(null, "Opción no válida.", "Error", JOptionPane.ERROR_MESSAGE);
+                                break;
                         }
                     }
-                
+                break;
             case 2: //Clientes
                     int submenuSeleccion1 = -1; // Inicializa la selección del submenú con un valor no válido
 
@@ -72,39 +98,94 @@ public class ModuloReportes {
                         switch (submenuSeleccion1) {
                             case 0: // Clientes registrados
                                 Lista_Clientes clientes=Lista_Clientes.leerClientes();
-                                clientes.getCabeza().getDato().getNombre();
+                        //       clientes.getCabeza().getDato().getNombre();
+                                
+                                if (clientes.getCabeza() == null) {
+                                    System.out.println("La lista está vacía.");
+                                 return;
+                                }else
+                                {
+                                    System.out.println(clientes.toString());
+                                }                
                               
                                 break;
                             case 1:// Clientes que compraron vehiculo
                                 ListaVehiculos comprados=ListaVehiculos.leerVehiculos();
-                                comprados.getCabeza().getDato().getEstado();
+                           // comprados.getCabeza().getDato().getEstado();
+                                //implementar un conteins para que no se repita si un cliente compró varios vehiculos
+                                if (comprados.getCabeza().getDato().getCliente() == null) {
+                                    System.out.println("La lista de clientes está vacía.");
+                                 return;
+                                }else
+                                {
+                                   
+                                }   
+                                
+                                
+                                
                                
                                 break;
                             case 2: // Clientes que reservaron vehiculo
                                 ListaVehiculos reservados=ListaVehiculos.leerVehiculos();
-                                reservados.getCabeza().getDato().getEstado();
+                          //      reservados.getCabeza().getDato().getEstado();
+                                //implementar un conteins para que no se repita si un cliente compró varios vehiculos
+                                if (reservados.getCabeza().getDato().getEstado()== null) {
+                                    System.out.println("La lista de clientes con reservas está vacía.");
+                                 return;
+                                }else
+                                {
+                                   
+                                }
+                                     
                                
                                 break;            
                             case 3: // MostrarInfo
                                 InformacionEmpresa.mostrarInformacionEnDialogo();
                                 break;
-                            case 4: // Salir
+                            case 4: // volver
+                               // submenuSeleccion1=-1;
+                                break;
+                                
+                            default:
+                                     JOptionPane.showMessageDialog(null, "Opción no válida.", "Error", JOptionPane.ERROR_MESSAGE);
                                 break;
                         }
-                    }
-                
+                        
+                    }                                       
                 break;
-               case 3: //Top 3 clientes
-                    ListaVehiculos top=ListaVehiculos.leerVehiculos();
-                    top.getCabeza().getDato().getCliente();
+                   
+               case 3: //Top 3 clientes                  
+                //   ListaVehiculos top=ListaVehiculos.leerVehiculos();
+                //   top.getCabeza().getDato().getCliente();
+                //Como saber cuantos vehiculos tiene un cliente?             
+                   ListaVehiculos top = ListaVehiculos.leerVehiculos();
+                    if (top.getCabeza() == null) {
+                         System.out.println("La lista está vacía.");
+                        return;
+                              }
+                  
+                   
+            
                     
                     break;
                 case 4: //Ventas por vendedor
                     //esto va con el modulo de Login
+                    Usuario.Lista_Usuario ventasVendedor = Usuario.Lista_Usuario.leerUsuarios();
+                   // if (ventasVendedor.getCabeza().getDato().)
+                    //debe tener un contador de ventas
+                    
+                    
+                    
                     
                     break;
                 case 5:  //Reservas por vendedor
                     //esto va con el modulo de Login
+                    Usuario.Lista_Usuario reservasVendedor = Usuario.Lista_Usuario.leerUsuarios();
+                    //debe tener un contador de reservas
+                    //con la lista de vehículo, crear una lista aux que no guarde
+                    //iterar con la lista de lo que salga en reservado y hacer un push a la auxiliar(get dato)
+                    
+                    
                     
                     break;                  
                 case 6:
@@ -117,8 +198,6 @@ public class ModuloReportes {
                     JOptionPane.showMessageDialog(null, "Opción no válida.", "Error", JOptionPane.ERROR_MESSAGE);
                     break;    
             }
-        }while(op!=8);
-    }
-        
-        
+        }while(op>=8);         
+    }        
     }
